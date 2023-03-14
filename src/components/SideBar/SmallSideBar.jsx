a8ac738b6667da2aefa9793e30040db8832be4f0
kim-seonkyu <seonkyu93@gmail.com>
2023-03-14 21:50:21 +0900

    sidebar ui & styling

--- src/components/SideBar/SmallSideBar.jsx
+++ src/components/SideBar/SmallSideBar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { FaCompass } from "react-icons/fa";
+import { MdSubscriptions } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const SmallSideBar = () => {
-  return <div>SmallSideBar</div>;
+  return (
+    <aside className="small_sidebar">
+      <div className="small_sidebar_container">
+        <Link to="/" data-for="sidebar" data-tip="Home">
+          <div className="sidebar_icon_container">
+            <AiFillHome size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Home</h4>
+          </div>
+        </Link>
+
+        <Link to="/feed/explore" data-for="sidebar" data-tip="Explore">
+          <div className="sidebar_icon_container">
+            <FaCompass size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Explore</h4>
+          </div>
+        </Link>
+
+        <Link
+          to="/feed/subscriptions"
+          data-for="sidebar"
+          data-tip="Subscriptions"
+        >
+          <div className="sidebar_icon_container">
+            <MdSubscriptions size={23} className="sidebar_icon" />
+            <h4 className="sidebar_text">Subscriptions</h4>
+          </div>
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default SmallSideBar;
